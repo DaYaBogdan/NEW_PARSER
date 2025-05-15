9c346fa9878260cf19eaed7cc510c1fe0fb06bdb
Богдан Смирнов <bogsm228@gmail.com>
2025-05-15 16:57:30 +0300

    Парсер законченный, соответствующий языку

--- еуые/main.cpp
+++ еуые/main.cpp
@@ -6,46 +6,52 @@
 
 enum Relation { LESS, EQUAL, GREATER, NONE };
 
+struct workSteckToken
+{
+    std::string token;
+    Relation relation;
+};
+
 std::map<std::string, std::map<std::string, Relation>> precedenceTable =
 {
-    {"var", {{"id", LESS}, {";", NONE}, {"begin", LESS}, {"end", LESS}, {"for", NONE},
-             {"to", NONE}, {"do", LESS}, {":=", LESS}, {"#", LESS}, {"!", NONE}, {"&", NONE}, {"$", GREATER}}},
-
-    {"id", {{"id", NONE}, {";", LESS}, {"begin", GREATER}, {"end", GREATER}, {"for", NONE},
-            {"to", LESS}, {"do", GREATER}, {":=", LESS}, {"#", LESS}, {"!", LESS}, {"&", LESS}, {"$", NONE}}},
-
-    {";", {{"id", LESS}, {";", NONE}, {"begin", GREATER}, {"end", NONE}, {"for", LESS},
-           {"to", NONE}, {"do", LESS}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
-
-    {"begin", {{"id", LESS}, {";", NONE}, {"begin", NONE}, {"end", GREATER}, {"for", GREATER},
-               {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
-
-    {"end", {{"id", NONE}, {";", NONE}, {"begin", NONE}, {"end", EQUAL}, {"for", NONE},
+    {"var", {{"id", LESS}, {";", NONE}, {"begin", LESS}, {"end", NONE}, {"for", NONE},
              {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", GREATER}}},
 
-    {"for", {{"id", GREATER}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
-             {"to", GREATER}, {"do", GREATER}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
+    {"id", {{"id", NONE}, {";", EQUAL}, {"begin", GREATER}, {"end", GREATER}, {"for", NONE},
+            {"to", GREATER}, {"do", EQUAL}, {":=", EQUAL}, {"#", EQUAL}, {"!", NONE}, {"&", EQUAL}, {"$", NONE}}},
 
-    {"to", {{"id", LESS}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
-            {"to", NONE}, {"do", GREATER}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
+    {";", {{"id", EQUAL}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", GREATER},
+           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
 
-    {"do", {{"id", NONE}, {";", NONE}, {"begin", GREATER}, {"end", NONE}, {"for", NONE},
+    {"begin", {{"id", LESS}, {";", NONE}, {"begin", NONE}, {"end", EQUAL}, {"for", LESS},
+               {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
+
+    {"end", {{"id", NONE}, {";", NONE}, {"begin", NONE}, {"end", GREATER}, {"for", NONE},
+             {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", GREATER}}},
+
+    {"for", {{"id", LESS}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
+             {"to", EQUAL}, {"do", EQUAL}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
+
+    {"to", {{"id", EQUAL}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
             {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
 
-    {":=", {{"id", LESS}, {";", NONE}, {"begin", NONE}, {"end", GREATER}, {"for", NONE},
-            {"to", NONE}, {"do", GREATER}, {":=", NONE}, {"#", GREATER}, {"!", LESS}, {"&", NONE}, {"$", NONE}}},
+    {"do", {{"id", NONE}, {";", NONE}, {"begin", LESS}, {"end", GREATER}, {"for", NONE},
+            {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
 
-    {"#", {{"id", GREATER}, {";", NONE}, {"begin", NONE}, {"end", GREATER}, {"for", NONE},
-           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", LESS}, {"&", NONE}, {"$", NONE}}},
+    {":=", {{"id", EQUAL}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
+            {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", EQUAL}, {"&", NONE}, {"$", NONE}}},
 
-    {"!", {{"id", LESS}, {";", NONE}, {"begin", NONE}, {"end", GREATER}, {"for", NONE},
-           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", LESS}, {"$", NONE}}},
+    {"#", {{"id", EQUAL}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
+           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", EQUAL}, {"&", NONE}, {"$", NONE}}},
 
-    {"&", {{"id", LESS}, {";", NONE}, {"begin", LESS}, {"end", GREATER}, {"for", NONE},
-           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", GREATER}, {"&", NONE}, {"$", NONE}}},
+    {"!", {{"id", EQUAL}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
+           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
 
-    {"$", {{"var", LESS}, {"id", NONE}, {";", NONE}, {"begin", NONE}, {"end", GREATER}, {"for", NONE},
-           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", EQUAL}}}
+    {"&", {{"id", EQUAL}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
+           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}},
+
+    {"$", {{"var", LESS}, {"id", NONE}, {";", NONE}, {"begin", NONE}, {"end", NONE}, {"for", NONE},
+           {"to", NONE}, {"do", NONE}, {":=", NONE}, {"#", NONE}, {"!", NONE}, {"&", NONE}, {"$", NONE}}}
 };
 
 
@@ -102,80 +108,90 @@ Relation getRelation(const std::string& top, const std::string& current) {
 }
 
 bool parse2(const std::vector<std::string>& tokens) {
-    std::stack<std::string> parseStack;
-    parseStack.push("$");
+    std::stack<workSteckToken> parseStack;
+    parseStack.push({ "$", NONE });
 
     size_t i = 0;
     while (i < tokens.size()) {
-        std::string current = tokens[i];
-        std::cout << "Current token: " << current << "\n";
+
+        workSteckToken current;
+        current.token = tokens[i];
+
+        //Visual part
+        std::cout << "Current token: " << current.token << "\n";
 
         if (parseStack.empty()) {
-            std::cout << "Error: stack is empty before processing token '" << current << "'\n";
+            std::cout << "Error: stack is empty before processing token '" << current.token << "'\n";
             return false;
         }
 
-        std::string top = parseStack.top();
-
-        if (current == "$" && top == "$") {
+        //Tokens end detection
+        workSteckToken top = parseStack.top();
+        if (current.token == "$" && top.token == "$") {
             std::cout << "End of parsing detected. Successful parsing.\n";
             return true;
         }
 
-        Relation relation = getRelation(top, current);
-        std::cout << "Relation between '" << top << "' and '" << current << "': "
-            << (relation == LESS ? "<" : relation == GREATER ? ">" : relation == EQUAL ? "=" : "NONE") << "\n";
+        //Relation identifier
+        current.relation = getRelation(top.token, current.token);
+        std::cout << "Relation between '" << top.token << "' and '" << current.token << "': "
+            << (current.relation == LESS ? "<" : current.relation == GREATER ? ">" : current.relation == EQUAL ? "=" : "NONE") << "\n";
 
-        if (!parseStack.empty() && parseStack.top() == "?" && current == ":") {
-            std::cout << "Reducing '?' with ':'\n";
-            parseStack.pop();
-        }
-
-        if (relation == LESS || relation == EQUAL) {
+        //Working block
+        if (current.relation == LESS || current.relation == EQUAL) {
             parseStack.push(current);
             i++;
-            std::cout << "Pushed '" << current << "' to stack\n";
+            std::cout << "Pushed '" << current.token << "' to stack\n";
         }
-        else if (relation == GREATER) {
-            do {
-                if (parseStack.empty()) {
+        else if (current.relation == GREATER) 
+        {
+            //Deleting GREATER token
+            std::cout << "Popped '" << parseStack.top().token << "' from stack\n";
+            parseStack.pop();
+
+            //Deleting all EQUAL tokens
+            while (!parseStack.empty() && parseStack.top().relation == EQUAL)
+            {
+                if (parseStack.empty())
+                {
                     std::cout << "Error: can't reduce - stack empty\n";
                     return false;
                 }
-                std::cout << "Popped '" << parseStack.top() << "' from stack\n";
+                std::cout << "Popped '" << parseStack.top().token << "' from stack\n";
                 parseStack.pop();
-            } while (!parseStack.empty() && getRelation(parseStack.top(), current) == GREATER);
+            }
 
-            if (!parseStack.empty() && getRelation(parseStack.top(), current) == NONE) {
-                std::cout << "Error: no relation after reduction between '" << parseStack.top() << "' and '" << current << "'\n";
-                return false;
+            //Deleting LESS token
+            if(parseStack.top().relation == LESS)
+            {
+                std::cout << "Popped '" << parseStack.top().token << "' from stack\n";
+                parseStack.pop();
             }
         }
-        else {
-            std::cout << "Syntax Error: no valid relation between '" << top
-                << "' and '" << current << "'\n";
+        else //Relation defined as NONE
+        {
+            std::cout << "Syntax Error: no valid relation between '" << top.token
+                << "' and '" << current.token << "'\n";
             return false;
         }
     }
-    while (!parseStack.empty() && parseStack.top() != "$") {
-        if (parseStack.top() == "?") {
-            std::cout << "Error: '?' must be reduced before finishing.\n";
-            return false;
-        }
-        std::cout << "Final reduction: popped '" << parseStack.top() << "'\n";
+
+    //Searching for errors
+    while (!parseStack.empty() && parseStack.top().token != "$") 
+    {
+        std::cout << "Final reduction: popped '" << parseStack.top().token << "'\n";
         parseStack.pop();
     }
 
-    return parseStack.size() == 1 && parseStack.top() == "$";
+    return parseStack.size() == 1 && parseStack.top().token == "$";
 }
 
 int main()
 {
-    std::string line = "var A;B;C begin A:=12 # !B&9 ; for B:=A to 8 do begin C:=! 5 end end";
+    std::string line = "var A;B;C begin A:=12 # !B&9 ; for B:=A to 8 do begin C:=!5 end end";
 
     std::cout << "Input Expression: ";
     //getline(std::cin, line);
-
 
     std::cout << parse2(tokenize(line));
 
